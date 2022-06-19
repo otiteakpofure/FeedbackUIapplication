1e5fd9538986aee91ec2eac9933c7664c3702f49
otiteakpofure <akpofure.otite@gmail.com>
2022-06-19 18:42:47 +0100

    added an input tag and state of my input application

--- feedbackui/src/components/FeedBackForm.jsx
+++ feedbackui/src/components/FeedBackForm.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function FeedBackForm() {
+    {/*Added State to the function */}
+    const [text, setText]=useState("")
+
+    const handleTextChange=(e)=>{
+        setText(e.target.value)
+    }
   return (
+    // form and all contents reagding the feedbackform items 
     <div style={{color:"black", fontSize:"1.5rem", marginBottom:"15px"}}>
         <div class="card">
+            {/* This is for the form in the feedback setup */}
                     <div class="card-body">
-                        This is some text within a card body.
+                        
+                        <form>
+                            <h2>How would you rate your services with us?</h2>
+                            {/*This is for the rating section in which users can put their scores on our services */}
+                            <div className="input-group">
+                                <input type="text" placeholder="write a review" onChange={handleTextChange} value={text} />
+                                <button className="btn btn btn-outline-dark " type="submit">send</button>
+                            </div>
+                        </form>
                     </div>
         </div>
     </div>
